5cb58ab9cfa31926fbe70ce6a4194ff0e6b57072
ShaibonJpeg <shaibonin@gmail.com>
2020-05-25 00:51:31 +0300

    Updates

---
+++ precache-manifest.8423463cee5895415c46012b67bd82be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bfdf94d1893762f20ca6eb818b352ba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f5f17ee9b304327f7a20",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "6e459376c9d8efb5ba32",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "f5f17ee9b304327f7a20",
+    "url": "./static/js/2.0f88db2a.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.0f88db2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e459376c9d8efb5ba32",
+    "url": "./static/js/main.77f68cc6.chunk.js"
+  },
+  {
+    "revision": "e76739aa6bce28d57ac2",
+    "url": "./static/js/runtime-main.dc9add3a.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
