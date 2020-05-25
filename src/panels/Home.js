9a9b2be719ceef1a065fc066cc2f9bc2c9e09c94
ShaibonJpeg <shaibonin@gmail.com>
2020-05-25 16:00:24 +0300

    add magicball

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -8,17 +8,40 @@ import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
 import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
 
+import blue from '../img/blue.png';
+import green from '../img/green.png';
+import magicball from '../img/Magic_ball.png';
+import red from '../img/red.png';
+import yellow from '../img/yellow.png';
+
 function handleClick() {
-	var arr = [
-							"first",
-							"second",
-							"third",
-							"foure"
-						];
+	const posts = [
+		["Положительно","Бесспорно"],
+		["Положительно","Предрешено"],
+		["Положительно","Никаких сомнений"],
+		["Положительно","Определённо да"],
+		["Положительно","Можешь быть уверен в этом"],
+		["Нерешительно положительно","Мне кажется — «да»"],
+		["Нерешительно положительно","Вероятнее всего"],
+		["Нерешительно положительно","Хорошие перспективы"],
+		["Нерешительно положительно","Знаки говорят — «да»"],
+		["Нерешительно положительно","Да"],
+		["Нейтральные","Пока не ясно, попробуй снова"],
+		["Нейтральные","Спроси позже"],
+		["Нейтральные","Лучше не рассказывать"],
+		["Нейтральные","Сейчас нельзя предсказать"],
+		["Нейтральные","Сконцентрируйся и спроси опять"],
+		["Отрицательные","Даже не думай"],
+		["Отрицательные","Мой ответ — «нет»"],
+		["Отрицательные","По моим данным — «нет»"],
+		["Отрицательные","Перспективы не очень хорошие"],
+		["Отрицательные","Весьма сомнительно"]
+	];
+
 	var min = 0;
-	var max = 3;
-	let rand = Math.random() * (4);
-	document.getElementById("textID").textContent = arr[Math.floor(rand)];
+	var max = 19;
+	let rand = min + Math.random() * (max + 1 - min);
+	document.getElementById("textID").textContent = posts[Math.floor(rand)][1];
 }
 
 const Home = ({ id, go, fetchedUser }) => (
@@ -43,6 +66,9 @@ const Home = ({ id, go, fetchedUser }) => (
 					Random
 				</Button>
 				<p id="textID">textID</p>
+				<div style="background:{blue} no-repeat;height:imageheight px;width:imagewidth px">
+<img src={magicball} alt="Persik The Cat"/>
+				</div>
 			</Div>
 		</Group>
 	</Panel>
